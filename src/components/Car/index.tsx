4ea0aec102787ce18b0fd0ed1da9fe5edd9ad52b
junior.marques <junior.marques@deixanoazul.com.br>
2022-08-25 22:00:51 -0300

    Create Component Car

---
+++ src/components/Car/index.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+
+import {
+  Container,
+  Datails,
+  Brand,
+  Name,
+  About,
+  Rent,
+  Period,
+  Price,
+  Type,
+  CarImage,
+} from './styles';
+
+import GasolineSvg from '../../assets/gasoline.svg';
+
+interface CarData {
+  brand: string;
+  name: string;
+  rent: {
+    period: string;
+    price: number;
+  };
+  thumbnail: string;
+}
+
+interface Props {
+  data: CarData;
+}
+
+export function Car({ data }: Props) {
+  return (
+    <Container>
+      <Datails>
+        <Brand>{data.brand}</Brand>
+        <Name>{data.name}</Name>
+
+        <About>
+          <Rent>
+            <Period>{data.rent.period}</Period>
+            <Price>R$ {data.rent.price}</Price>
+          </Rent>
+
+          <Type>
+            <GasolineSvg />
+          </Type>
+        </About>
+      </Datails>
+
+      <CarImage
+        source={{
+          uri: data.thumbnail,
+        }}
+        resizeMode="contain"
+      />
+    </Container>
+  );
+}
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,6 +3,7 @@ import { StatusBar } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 
 import Logo from '../../assets/logo.svg';
+import { Car } from '../../components/Car';
 
 import {
   Container,
@@ -12,6 +13,26 @@ import {
 } from './styles';
 
 export function Home() {
+  const carData = {
+    brand: 'Audi',
+    name: 'RS 5 Coupé',
+    rent: {
+      period: 'AO DIA',
+      price: 120,
+    },
+    thumbnail: 'https://pngimg.com/uploads/audi/audi_PNG99491.png',
+  };
+  const carDataTwo = {
+    brand: 'Audi',
+    name: 'Q8',
+    rent: {
+      period: 'AO DIA',
+      price: 300,
+    },
+    thumbnail:
+      'https://production.autoforce.com/uploads/version/profile_image/6732/model_main_comprar-performance-55-tfsi-quattro-tiptronic_bd8e97334c.png',
+  };
+
   return (
     <Container>
       <StatusBar
@@ -26,6 +47,9 @@ export function Home() {
           <TotalCars>Total de 12 carros</TotalCars>
         </HeaderContent>
       </Header>
+
+      <Car data={carData} />
+      <Car data={carDataTwo} />
     </Container>
   );
 }
