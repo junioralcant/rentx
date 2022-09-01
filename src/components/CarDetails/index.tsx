7b400f54f80f4f24bdd10167501a8a523e63a466
junioralcant <junioralcant111@gmail.com>
2022-09-01 14:44:18 -0300

    Create content in component Slider

--- src/components/CarDetails/index.tsx
+++ src/components/CarDetails/index.tsx
@@ -2,7 +2,20 @@ import React from 'react';
 import { BackButton } from '../BackButton';
 import { ImageSlider } from '../ImageSlider';
 
-import { Container, Header, CardImage } from './styles';
+import {
+  Container,
+  Header,
+  CardImage,
+  Content,
+  Details,
+  Brand,
+  Rent,
+  Period,
+  Price,
+  Description,
+  Name,
+  About,
+} from './styles';
 
 export function CarDetails() {
   return (
@@ -18,6 +31,27 @@ export function CarDetails() {
           ]}
         />
       </CardImage>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Lamborghini</Brand>
+            <Name>Huracan</Name>
+          </Description>
+
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 580</Price>
+          </Rent>
+        </Details>
+
+        <About>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Pariatur quis neque quasi, aliquam beatae asperiores eum
+          cumque a. Ex magni dolor quasi quam vitae fuga impedit iure
+          quaerat quos esse.
+        </About>
+      </Content>
     </Container>
   );
 }
